924c982ee0b3243bb213c1aa29bfb54c818e5009
denis ramos <pacohelmer@gmail.com>
2024-04-24 14:25:30 -0400

    pagina6

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './componentes/Home.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -9,7 +10,7 @@ import {
 
 ReactDOM.render(
   <Router basename="/my-proyecto-con-react"> {/* Aquí especificamos el basename */}
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={Home} />
     {/* Otras rutas de tu aplicación */}
   </Router>,
   document.getElementById('root')
