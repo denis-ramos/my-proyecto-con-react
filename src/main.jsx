3a007c6a0bcb389896f893a7cfe073334cba6ad5
denis ramos <pacohelmer@gmail.com>
2024-04-24 14:38:03 -0400

    page8

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Home from './componentes/Home.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename="/my-proyecto-con-react"> {/* Aquí especificamos el basename */}
-    <Route exact path="/" component={Home} />
-    {/* Otras rutas de tu aplicación */}
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
