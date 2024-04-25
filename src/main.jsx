ab1aa6f2470e59045ff362ef12fad93dfcfeba97
denis ramos <pacohelmer@gmail.com>
2024-04-24 22:22:46 -0400

    pagefinal

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, Route, Router, RouterProvider } from "react-router-dom";
+import Rickandmortyapp from "./componentes/Rickandmortyapp.jsx";
+import Home from "./componentes/Home.jsx";
 
+import { createBrowserRouter, createHashRouter, Route, Router, RouterProvider } from "react-router-dom";
+import Home from "./componentes/Home.jsx";
+
+const router =createHashRouter([
+  {
+    path: "/",
+    element:<Home/>
+  },
+  {
+    path: "/",
+    element:<Rickandmortyapp/>
+  },
+  {
+    path: "/*",
+    element:<p>ERROR</p>
+  }
+]
+)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
